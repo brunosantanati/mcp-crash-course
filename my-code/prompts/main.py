4c1ddf7c780468294c7b97005192433a20ee4df9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2026-09-13 14:58:06 -0300

    Add some comments

--- my-code/prompts/main.py
+++ my-code/prompts/main.py
@@ -13,7 +13,14 @@ def get_research_prompt(topic: str) -> str:
 
 
 if __name__ == "__main__":
+    # Print the docstring
     # print(get_research_prompt.__doc__)
     # print(get_research_prompt.fn.__doc__)
-    # mcp.run(transport="http") # Log -> ERROR:    Cancel 0 running task(s), timeout graceful shutdown exceeded
-    mcp.run(transport="stdio")
+
+    # mcp.run(transport="stdio") # Integrate with Claud Desktop
+    mcp.run(transport="http") # Test with Inspector
+
+    # I had to use Node 22 in order to execute the Inspector:
+    # nvm install 22
+    # nvm use 22
+    # npx -y @modelcontextprotocol/inspector
